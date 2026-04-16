eaab09b602f9ea30cd9d50d2b50e6a5d3911fe41
unknown <fini.ardiyanti@gmail.com>
2026-04-16 13:13:46 +0700

    implement determine winner and close expired auctions

--- src/main/java/id/ac/ui/cs/advprog/bidmart/bidding/controller/BiddingController.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmart/bidding/controller/BiddingController.java
@@ -29,20 +29,4 @@ public class BiddingController {
     ) {
         return biddingService.placeBid(userId, auctionId, amount);
     }
-
-    @PostMapping("/win")
-    public String winAuction(
-            @RequestParam String userId,
-            @RequestParam Double amount
-    ) {
-        return biddingService.winAuction(userId, amount);
-    }
-
-    @PostMapping("/lose")
-    public String loseAuction(
-            @RequestParam String userId,
-            @RequestParam Double amount
-    ) {
-        return biddingService.loseAuction(userId, amount);
-    }
 }
