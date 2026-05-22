a4d3434e077918ded4520f2619e4d675db29ee20
unknown <fini.ardiyanti@gmail.com>
2026-05-22 19:24:17 +0700

    resolve failed

--- src/main/java/id/ac/ui/cs/advprog/bidmart/bidding/entity/Auction.java
+++ src/main/java/id/ac/ui/cs/advprog/bidmart/bidding/entity/Auction.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.bidmart.bidding.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,6 +13,7 @@ import java.util.List;
 @Getter
 @Setter
 @NoArgsConstructor
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Auction {
 
     @Id
